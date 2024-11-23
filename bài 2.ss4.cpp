b9a26e3355fa0503a7f238e3fbca3727a54a276d
An256122006 <dinhtrongan2006@gmail.com>
2024-11-23 22:06:54 +0700

    Add files via upload

---
+++ bài 2.ss4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf ("moi nhap gia tri :");
+	scanf ("%d",&a);
+	if (a%2==0)
+	{
+		printf ("la so chan"); 
+	 } else {
+	 	printf ("la so le"); 
+	 } 
+	 return 0; 
+ } 
